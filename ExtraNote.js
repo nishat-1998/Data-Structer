514d40e1579b27614e29d85fd6f4737e15e67ef5
Nishat Ahmed <nishatahmed4485@gmail.com>
2022-07-27 00:11:55 +0600

    five

--- ExtraNote.js
+++ ExtraNote.js
@@ -1,5 +1,17 @@
-function 
+
 // Big O Notaion
+
+   // Big O(n) -- (5n+2)
+
+   function sumAll(n){
+    let total=0;
+    for(let i= 0; i<= n; i++) {
+        total += i
+    }
+    return total
+}
+//Big O(1)
+function 
 sumAll(n){
     return n *(n+1)/2
  }
@@ -8,3 +20,29 @@ sumAll(n){
  let time2 = performance.now()
  
  console.log(`your time ${(time2-time1)/1000 }`)
+
+ // Big O(n2)-- (n+n)
+
+ function multipleArray(n) {
+    for (let i = 0; i <= n; i++){
+    for (let j=0; j<=n; j++){
+        console.log(i,j)
+    }
+    }
+    }
+   
+   multipleArray(5)
+
+// Big O(3)---(n+3)
+
+function multipleArray(n) {
+    for (let i = 0; i <= n; i++){
+    for (let j=0; j<= 3; j++){
+        console.log(i,j)
+    }
+    }
+    }
+   
+   multipleArray(5)
+
+
